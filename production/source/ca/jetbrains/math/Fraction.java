5a617c02b4f3f31f8f7d005adbde782e15a746c9
kenneth <venki6@SGMUOB03JLVCF.local>
2020-02-28 18:34:33 +0800

    added numerator and denominator to work with plus method

--- production/source/ca/jetbrains/math/Fraction.java
+++ production/source/ca/jetbrains/math/Fraction.java
@@ -2,15 +2,34 @@ package ca.jetbrains.math;
 
 public class Fraction {
     private int integerValue;
+    private int denominator;
+    private int numerator;
+
+
+
+    public int getNumerator() {
+        return numerator;
+    }
+    public int getDenominator() {
+        return denominator;
+    }
 
     public Fraction(int integerValue) {
 
         this.integerValue = integerValue;
     }
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
 
     public Fraction plus(Fraction that) {
 
         return new Fraction(  this.integerValue + that.integerValue);
+    }
+    public Fraction plus (int numerator, int denominator) {
+        this.numerator += numerator;
+        return new Fraction(this.numerator, denominator);
     }
 
     public int intValue() {
--- test/source/ca/jetbrains/math/test/addFractionTest.java
+++ test/source/ca/jetbrains/math/test/addFractionTest.java
@@ -1,6 +1,7 @@
 package ca.jetbrains.math.test;
 
 import ca.jetbrains.math.Fraction;
+import com.sun.xml.internal.ws.api.addressing.OneWayFeature;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -27,7 +28,20 @@ public class addFractionTest {
 
     @Test
     public void nonNegativeNonZeroOperands() throws Exception{
-        Fraction sum = new Fraction(3).plus(new Fraction(4));
+        final Fraction sum = new Fraction(3).plus(new Fraction(4));
         assertEquals(7, sum.intValue());
+    }
+
+    @Test
+    public void negativeInputAndNegativeOutput() throws Exception{
+        final Fraction sum = new Fraction(-3).plus(new Fraction(1));
+        assertEquals(-2, sum.intValue());
+    }
+
+    @Test
+    public void nonTrivialDenominator() throws Exception{
+        final Fraction sum = new Fraction(1,5).plus(2,5);
+        assertEquals(3, sum.getNumerator());
+        assertEquals(5, sum.getDenominator());
     }
 }
