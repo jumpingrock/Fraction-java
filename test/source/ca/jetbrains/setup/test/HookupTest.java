80fd42207f9fbcf01dc026e0b3bbf6514ee081aa
kenneth <venki6@SGMUOB03JLVCF.local>
2020-02-28 13:39:04 +0800

    idea testrunner configuration for one button testing

---
+++ test/source/ca/jetbrains/setup/test/HookupTest.java
@@ -0,0 +1,12 @@
+package ca.jetbrains.setup.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HookupTest {
+    @Test
+    public void isThisThingOn() throws Exception {
+
+//        Assert.fail("not yet implemented ");
+    }
+}
