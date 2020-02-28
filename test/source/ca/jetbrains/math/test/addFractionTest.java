3c1dc98894da8a02114ead9ed72aad572bffef9f
kenneth <venki6@SGMUOB03JLVCF.local>
2020-02-28 15:15:26 +0800

    adding zero to zero test pass

---
+++ test/source/ca/jetbrains/math/test/addFractionTest.java
@@ -0,0 +1,17 @@
+package ca.jetbrains.math.test;
+
+import ca.jetbrains.math.Fraction;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class addFractionTest {
+    @Test
+    public void zeroPlusZero() throws Exception {
+        Fraction sum = new Fraction(0).plus(new Fraction(0));
+        assertEquals(0, sum.intValue());
+    }
+
+
+}
