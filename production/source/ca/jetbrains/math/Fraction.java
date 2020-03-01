1b5ab0e70f20caf3be88bbba27b605b8be321ee3
jumpingrock <kennethyeongky@gmail.com>
2020-03-01 15:36:10 +0800

    removed intValue and replaced with numerator, all test pass

--- production/source/ca/jetbrains/math/Fraction.java
+++ production/source/ca/jetbrains/math/Fraction.java
@@ -1,7 +1,6 @@
 package ca.jetbrains.math;
 
 public class Fraction {
-    private int integerValue;
     private int denominator;
     private int numerator;
 
@@ -14,27 +13,26 @@ public class Fraction {
         return denominator;
     }
 
-    public Fraction(int integerValue) {
+    public Fraction(int numerator) {
 
-        this.integerValue = integerValue;
+        this.numerator = numerator;
+        this.denominator = 1;
     }
+
     public Fraction(int numerator, int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
     }
 
     public Fraction plus(Fraction that) {
-
-        return new Fraction(  this.integerValue + that.integerValue);
+        return new Fraction(  this.numerator + that.numerator, denominator);
     }
+
     public Fraction plus (int numerator, int denominator) {
         this.numerator += numerator;
         return new Fraction(this.numerator, denominator);
     }
 
-    public int intValue() {
-        return integerValue;
-    }
 
 
 }
