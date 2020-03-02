19d72ecd547ed9ff510d426796ca5211d444ea67
kenneth <venki6@SGMUOB03JLVCF.local>
2020-03-02 12:15:25 +0800

    implemented gcd in gcd test before moving it to Fractions

---
+++ test/source/ca/jetbrains/math/test/reduceFractionTest.java
@@ -0,0 +1,21 @@
+package ca.jetbrains.math.test;
+
+import ca.jetbrains.math.Fraction;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class reduceFractionTest {
+    @Test
+    public void alreadyInLowestTerms() {
+    assertEquals(new Fraction(3,4), new Fraction(3,4));
+    //we already test this in fractionEqualTest
+    }
+
+    @Test
+    @Ignore("waiting to implement gcd")
+    public void reduceToNotWholeNumber() {
+        assertEquals(new Fraction(3,4), new Fraction(6,8));
+    }
+}
