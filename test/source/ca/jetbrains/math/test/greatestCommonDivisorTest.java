983b9a4c0060fe68dd987191ff2890bdedc56c61
kenneth <venki6@SGMUOB03JLVCF.local>
2020-03-02 15:31:42 +0800

    added gcd and tested wikipedia algo found a annoying bug that is fixed by using Math.abs() on output

--- test/source/ca/jetbrains/math/test/greatestCommonDivisorTest.java
+++ test/source/ca/jetbrains/math/test/greatestCommonDivisorTest.java
@@ -2,13 +2,45 @@ package ca.jetbrains.math.test;
 
 import org.junit.Test;
 
+import static java.lang.Math.abs;
 import static org.junit.Assert.assertEquals;
 
 public class greatestCommonDivisorTest {
 
     @Test
-    public void oneAndOne() {
-        assertEquals(1, gcd(1,1));
+    public void reflexiveExamples() {
+        assertEquals(9, gcd(9,18));
+        assertEquals(2, gcd(2,2));
+        assertEquals(1, gcd(-1,-1));
+    }
+
+    @Test
+    public void relativelyPrime() {
+        assertEquals(1, gcd(2,3));
+        assertEquals(1, gcd(4,7));
+        assertEquals(1, gcd(-2,-3));
+    }
+
+    @Test
+    public void oneIsMultipleOfTheOther() {
+        assertEquals(3, gcd(3,9));
+        assertEquals(5, gcd(5,30));
+
+    }
+
+    @Test
+    public void commonFactor() {
+        assertEquals(2,gcd(6,8));
+        assertEquals(7,gcd(49,315));
+        assertEquals(4,gcd(-24,-28));
+
+    }
+
+    @Test
+    public void negatives() {
+        assertEquals(4,gcd(-24,28));
+        assertEquals(4,gcd(24,-28));
+
     }
 
     private int gcd(int a, int b) {
@@ -17,6 +49,6 @@ public class greatestCommonDivisorTest {
             b = a % t;
             a = t;
         }
-        return a;
+        return Math.abs(a);
     }
 }
